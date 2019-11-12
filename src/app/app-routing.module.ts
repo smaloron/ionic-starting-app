f1c99c98cb6f9d1c62ee333aaecae3cc802f2da3
anonymous <moi@moi.com>
2019-11-12 14:11:07 +0100

    ajout de la page /form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'form', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'form', loadChildren: './pages/form/form.module#FormPageModule' },
 ];
 
 @NgModule({
